8db619d57aea47724914c83d996a1a2bb1303c4e
Lisbeth5 <araylisbeth5@gmail.com>
2026-09-08 23:35:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aplicacin_movil_de_gestin_de_procesos_electorales/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.aplicacin_movil_de_gestin_de_procesos_electorales
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aplicacin_movil_de_gestin_de_procesos_electorales.ui.theme.Aplicación_Movil_De_Gestión_De_Procesos_electoralesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Aplicación_Movil_De_Gestión_De_Procesos_electoralesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Aplicación_Movil_De_Gestión_De_Procesos_electoralesTheme {
+        Greeting("Android")
+    }
+}
